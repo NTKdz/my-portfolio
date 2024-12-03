c47bb35a858e5d1de7be518fa9932691e9c58c32
NTKdz <nguyenthekhoi2003@gmail.com>
2024-12-03 22:13:52 +0700

    build site

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+import { useNavigate } from "react-router";
 import "./styles.css";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div className="h-full flex flex-col justify-center gap-8 pl-8 relative overflow-hidden">
       <div id="text-animation" className="text-7xl">
         Khoi Nguyen The
       </div>
-      <div className="uppercase text-xl text-blue-500">
-        fullstack developer
-      </div>
+      <div className="uppercase text-xl text-blue-500">fullstack developer</div>
       <div className="flex gap-6">
-        <button className="bg-transparent hover:bg-blue-500 text-blue-300 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-300 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={() => {
+            navigate("/projects");
+          }}
+        >
           View projects
         </button>
-        <button className="bg-transparent hover:bg-blue-500 text-blue-300 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-300 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={() => navigate("/contact")}
+        >
           Contact me
         </button>
       </div>
